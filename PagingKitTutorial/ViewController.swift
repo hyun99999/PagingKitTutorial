cf96952c1b8ea802c4d35cbcf3df2c4919cf85c1
hyungyu Kim <ypsophila97@icloud.com>
2021-01-29 17:37:57 +0900

    Install PagingKit Pod and Add init Main.stroyboard

--- PagingKitTutorial/ViewController.swift
+++ PagingKitTutorial/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import PagingKit
 
 class ViewController: UIViewController {
-
+    
+    var menuViewController: PagingMenuViewController!
+    var contentViewController: PagingContentViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           if let vc = segue.destination as? PagingMenuViewController {
+               menuViewController = vc
+           } else if let vc = segue.destination as? PagingContentViewController {
+               contentViewController = vc
+           }
+       }
 
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PagingKitTutorial' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PagingKitTutorial
+pod "PagingKit"
+end
