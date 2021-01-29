39c1b3b53ccd855ea832a030a3f298934000beec
hyungyu Kim <ypsophila97@icloud.com>
2021-01-29 17:49:14 +0900

    Add MenuCell

---
+++ PagingKitTutorial/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  PagingKitTutorial
+//
+//  Created by kimhyungyu on 2021/01/29.
+//
+
+import UIKit
+import PagingKit
+
+class MenuCell: PagingMenuViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+}
